21564984ab2d52a226b759d79c48f19274704c52
Hexilee <hexileee@gmail.com>
2020-01-21 15:18:30 +0800

    add futures

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,31 @@
+use futures::future;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Error, Request, Response, Server};
 use std::convert::Infallible;
 use std::future::Future;
-use std::marker::PhantomData;
 use std::net::SocketAddr;
 use std::pin::Pin;
-use std::sync::Arc;
 
 pub type MiddlewareStatus<'a> =
     Pin<Box<dyn 'a + Future<Output = Result<(), Infallible>> + Sync + Send>>;
 
-pub type Next<'a, D> =
-    Box<dyn 'a + for<'b> FnOnce(&'b mut Context<D>) -> MiddlewareStatus<'b> + Sync + Send>;
+pub type Next<D> = dyn for<'b> Fn(&'b mut Context<D>) -> MiddlewareStatus<'b> + Sync + Send;
 
 pub trait Middleware<D>:
-    Sync + Send + for<'a> Fn(&'a mut Context<D>, Next<'a, D>) -> MiddlewareStatus<'a>
+    Sync + Send + for<'a> Fn(&'a mut Context<D>, &'a Next<D>) -> MiddlewareStatus<'a>
 {
 }
 impl<D, T> Middleware<D> for T where
-    T: Sync + Send + for<'a> Fn(&'a mut Context<D>, Next<'a, D>) -> MiddlewareStatus<'a>
+    T: Sync + Send + for<'a> Fn(&'a mut Context<D>, &'a Next<D>) -> MiddlewareStatus<'a>
 {
 }
 
 pub struct Roa<D: Default = ()> {
-    _data: PhantomData<D>,
-    middlewares: Vec<Box<dyn Middleware<D>>>,
+    handler: Box<dyn Middleware<D>>,
+}
+
+fn _next<D: Default>(_ctx: &mut Context<D>) -> MiddlewareStatus {
+    Box::pin(async { Ok(()) })
 }
 
 pub struct Context<D: Default = ()> {
@@ -35,32 +36,28 @@ pub struct Context<D: Default = ()> {
 impl<D: Default + Sync + Send + 'static> Roa<D> {
     pub fn new() -> Self {
         Self {
-            _data: PhantomData::<D>,
-            middlewares: Vec::new(),
+            handler: Box::new(|ctx, next| Box::pin(async { next(ctx).await })),
         }
     }
 
-    async fn handle(self: Arc<Self>, req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    pub fn gate(&mut self, middleware: impl Middleware<D>) {
+        unimplemented!()
+    }
+
+    async fn handle(&self, req: Request<Body>) -> Result<Response<Body>, Infallible> {
         let mut context = Context::new(req);
-        let mut handler: Next<D> = Box::new(|_ctx: &mut Context<D>| Box::pin(async { Ok(()) }));
-        for middleware in self.middlewares.iter().rev() {
-            handler = Box::new(move |ctx| Box::pin(async move { middleware(ctx, handler).await }));
-        }
-        handler(&mut context).await?;
+        (self.handler)(&mut context, &_next).await?;
         Ok(Response::new(Body::empty()))
     }
 
-    pub async fn listen(&self, addr: &SocketAddr) -> Result<(), Error> {
-        let origin = Arc::new(self.clone());
-        let make_svc = make_service_fn(move |_conn| {
-            let app = origin.clone();
-            let handler = move |req| {
-                let copied = app.clone();
-                async move { copied.handle(req).await }
-            };
-            async move { Ok::<_, Infallible>(service_fn(handler)) }
+    pub fn listen<'a>(
+        &'a self,
+        addr: &'a SocketAddr,
+    ) -> impl 'a + Future<Output = Result<(), Error>> {
+        let make_svc = make_service_fn(|_conn| {
+            future::ok::<_, Infallible>(service_fn(|req| self.handle(req)))
         });
-        Server::bind(addr).serve(make_svc).await
+        Server::bind(addr).serve(make_svc)
     }
 }
 
