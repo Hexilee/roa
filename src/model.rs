98dec5fe72c81a33da2da30afa4db0b94506240b
Hexilee <hexileee@gmail.com>
2020-01-26 19:38:48 +0800

    add Model

---
+++ src/model.rs
@@ -0,0 +1,19 @@
+pub trait State: 'static + Send + Sized {
+    type Model: Model<State = Self>;
+}
+
+pub trait Model: 'static + Send + Sync + Sized {
+    type State: State<Model = Self>;
+    fn new_state(&self) -> Self::State;
+}
+
+impl Model for () {
+    type State = ();
+    fn new_state(&self) -> Self::State {
+        ()
+    }
+}
+
+impl State for () {
+    type Model = ();
+}
