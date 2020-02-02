8e439a52bf32f6aea9360d9cd376ebde1b4cd45a
Hexilee <hexileee@gmail.com>
2020-02-02 11:40:11 +0800

    roa-cookie compiling pass

--- roa-cookie/src/lib.rs
+++ roa-cookie/src/lib.rs
@@ -5,37 +5,42 @@ use roa_core::{Context, Model, Status};
 
 #[async_trait]
 pub trait Cookier {
-    async fn cookies<'a>(&'a self) -> Result<Box<dyn Iterator<Item = Cookie<'a>> + 'a>, Status>;
+    async fn cookies(&self) -> Result<Vec<Cookie<'static>>, Status>;
     async fn cookie(&self, key: &str) -> Result<Option<Cookie<'static>>, Status> {
-        Ok(self.cookies()?.find(|cookie| cookie.name() == key))
+        Ok(self
+            .cookies()
+            .await?
+            .into_iter()
+            .find(|cookie| cookie.name() == key))
     }
-    async fn set_cookie(&mut self, cookie: &Cookie) -> Result<(), Status>;
+    async fn set_cookie(&self, cookie: Cookie<'_>) -> Result<(), Status>;
 }
 
 #[async_trait]
 impl<M: Model> Cookier for Context<M> {
-    async fn cookies<'a>(&'a self) -> Result<Box<dyn Iterator<Item = Cookie<'a>> + 'a>, Status> {
-        Ok(match self.req().await.headers.get(header::COOKIE) {
-            None => Box::new(vec![].into_iter()),
-            Some(cookies) => Box::new(
-                cookies
-                    .to_str()?
-                    .split(';')
-                    .map(Cookie::parse_encoded)
-                    .filter_map(|cookie| cookie.ok()),
-            ),
+    async fn cookies(&self) -> Result<Vec<Cookie<'static>>, Status> {
+        Ok(match self.header(&header::COOKIE).await {
+            None => vec![],
+            Some(cookies) => cookies
+                .to_str()?
+                .split(';')
+                .map(Cookie::parse_encoded)
+                .filter_map(|cookie| cookie.ok())
+                .map(|cookie| cookie.into_owned())
+                .collect(),
         })
     }
 
-    async fn set_cookie(&mut self, cookie: &Cookie) -> Result<(), Status> {
+    async fn set_cookie(&self, cookie: Cookie<'_>) -> Result<(), Status> {
         let cookie_value = cookie.encoded().to_string().parse()?;
-
-        if self.response.headers.contains_key(header::SET_COOKIE) {
-            self.response
+        if self.resp().await.headers.contains_key(header::SET_COOKIE) {
+            self.resp()
+                .await
                 .headers
                 .append(header::SET_COOKIE, cookie_value);
         } else {
-            self.response
+            self.resp()
+                .await
                 .headers
                 .insert(header::SET_COOKIE, cookie_value);
         }
