e16ba3a07a50bc1944ad3606878d1ac5aba4e799
Hexilee <hexileee@gmail.com>
2020-02-02 22:29:24 +0800

    move jwt into roa

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,13 +1,15 @@
 pub use async_trait::async_trait;
 pub use jsonwebtoken::Validation;
 
+use crate::{Context, DynTargetHandler, Model, Next, Status, StatusCode, TargetHandler};
 use http::header::{AUTHORIZATION, WWW_AUTHENTICATE};
 use http::HeaderValue;
-use jsonwebtoken::{dangerous_unsafe_decode, decode};
-use roa_core::{Context, Model, Next, Status, StatusCode};
+use jsonwebtoken::decode;
 use serde::{de::DeserializeOwned, Serialize};
 
-const INVALID_HEADER_VALUE: &str = r#"Bearer realm="<jwt>", error="invalid_token""#;
+const INVALID_TOKEN: &str = r#"Bearer realm="<jwt>", error="invalid_token""#;
+
+struct JwtSymbol;
 
 #[async_trait]
 pub trait JwtVerifier<M, C>
@@ -15,18 +17,14 @@ where
     M: Model,
     C: 'static + Serialize + DeserializeOwned,
 {
-    async fn get_validation(&mut self) -> Validation {
-        Validation::default()
-    }
-    async fn get_secret(&mut self, claim: &C) -> Result<Vec<u8>, Status>;
-    async fn set_claim(&mut self, claim: C);
+    async fn verify(&self, validation: &Validation) -> Result<C, Status>;
 }
 
 async fn unauthorized_error<M: Model, E: ToString>(
     ctx: &Context<M>,
     www_authentication: &'static str,
 ) -> impl Fn(E) -> Status {
-    ctx.resp().await.headers.insert(
+    ctx.resp_mut().await.headers.insert(
         WWW_AUTHENTICATE,
         HeaderValue::from_static(www_authentication),
     );
@@ -35,40 +33,67 @@ async fn unauthorized_error<M: Model, E: ToString>(
 
 async fn try_get_token<M: Model>(ctx: &Context<M>) -> Result<String, Status> {
     match ctx.header(&AUTHORIZATION).await {
-        None | Some(Err(_)) => {
-            Err((unauthorized_error(&mut ctx.clone(), INVALID_HEADER_VALUE).await)(""))
-        }
+        None | Some(Err(_)) => Err((unauthorized_error(&mut ctx.clone(), INVALID_TOKEN).await)(
+            "",
+        )),
         Some(Ok(value)) => match value.find("Bearer") {
-            None => Err((unauthorized_error(&mut ctx.clone(), INVALID_HEADER_VALUE).await)("")),
+            None => Err((unauthorized_error(&mut ctx.clone(), INVALID_TOKEN).await)(
+                "",
+            )),
             Some(n) => Ok(value[n + 6..].trim().to_string()),
         },
     }
 }
 
-pub async fn jwt_verify<M, C>(ctx: Context<M>, next: Next) -> Result<(), Status>
+pub fn jwt_verify<M, C>(secret: String, validation: Validation) -> Box<DynTargetHandler<M, Next>>
 where
     M: Model,
-    C: 'static + Serialize + DeserializeOwned,
-    M::State: JwtVerifier<M, C>,
+    C: 'static + Serialize + DeserializeOwned + Send,
 {
-    let token = try_get_token(&ctx).await?;
-    let dangerous_data = dangerous_unsafe_decode(&token)
-        .map_err(unauthorized_error(&ctx, INVALID_HEADER_VALUE).await)?;
-    let secret = ctx.state().await.get_secret(&dangerous_data.claims).await?;
-    let validation = ctx.state().await.get_validation().await;
-    let token_data = decode(&token, &secret, &validation)
-        .map_err(unauthorized_error(&ctx, INVALID_HEADER_VALUE).await)?;
-    ctx.state().await.set_claim(token_data.claims).await;
-    next().await
+    Box::new(move |ctx, next: Next| {
+        let secret = secret.clone();
+        let validation = validation.clone();
+        async move {
+            let token = try_get_token(&ctx).await?;
+            decode::<C>(&token, secret.as_bytes(), &validation)
+                .map_err(unauthorized_error(&ctx, INVALID_TOKEN).await)?;
+            ctx.store::<JwtSymbol>("secret", secret).await;
+            ctx.store::<JwtSymbol>("token", token).await;
+            next().await
+        }
+    })
+    .dynamic()
+}
+
+#[async_trait]
+impl<M, C> JwtVerifier<M, C> for Context<M>
+where
+    M: Model,
+    C: 'static + Serialize + DeserializeOwned + Send,
+{
+    async fn verify(&self, validation: &Validation) -> Result<C, Status> {
+        let secret = self.load::<JwtSymbol>("secret").await;
+        let token = self.load::<JwtSymbol>("token").await;
+        match (secret, token) {
+            (Some(secret), Some(token)) => decode(&token, secret.as_bytes(), &validation)
+                .map_err(unauthorized_error(self, INVALID_TOKEN).await)
+                .map(|data| data.claims),
+            _ => Err(Status::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "middleware `jwt_verify` is not set correctly",
+                false,
+            )),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{async_trait, jwt_verify, JwtVerifier, INVALID_HEADER_VALUE};
+    use super::{jwt_verify, JwtVerifier, Validation, INVALID_TOKEN};
     use http::header::{AUTHORIZATION, WWW_AUTHENTICATE};
     use http::{HeaderValue, StatusCode};
     use jsonwebtoken::{encode, Header};
-    use roa_core::{App, Model, Request, Status};
+    use crate::{App, Request};
     use serde::{Deserialize, Serialize};
     use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
@@ -81,59 +106,24 @@ mod tests {
         name: String,
     }
 
-    struct AppModel {
-        secret: Vec<u8>,
-    }
-
-    struct AppState {
-        user: Option<User>,
-        secret: Vec<u8>,
-    }
-
-    impl Model for AppModel {
-        type State = AppState;
-        fn new_state(&self) -> Self::State {
-            AppState {
-                user: None,
-                secret: self.secret.clone(),
-            }
-        }
-    }
-
-    #[async_trait]
-    impl JwtVerifier<AppModel, User> for AppState {
-        async fn get_secret(&self, _claim: &User) -> Result<Vec<u8>, Status> {
-            Ok(self.secret.clone())
-        }
-
-        async fn set_claim(&mut self, claim: User) {
-            self.user = Some(claim)
-        }
-    }
-
-    const SECRET: &[u8] = b"123456";
+    const SECRET: &str = "123456";
 
     #[tokio::test]
     async fn verify() -> Result<(), Box<dyn std::error::Error>> {
-        let mut app = App::new(AppModel {
-            secret: SECRET.to_vec(),
-        });
-        app.join(jwt_verify).join(move |ctx, _next| async move {
-            match ctx.state().await.user {
-                None => panic!("ctx.usr should not be None"),
-                Some(ref user) => {
-                    assert_eq!(0, user.id);
-                    assert_eq!("Hexilee", &user.name);
-                }
-            }
-            Ok(())
-        });
+        let mut app = App::new(());
+        app.join(jwt_verify::<(), User>(SECRET.to_string(), Validation::default()))
+            .join(move |ctx, _next| async move {
+                let user: User = ctx.verify(&Validation::default()).await?;
+                assert_eq!(0, user.id);
+                assert_eq!("Hexilee", &user.name);
+                Ok(())
+            });
         let addr = "127.0.0.1:8000".parse()?;
         // no header value
         let resp = app.serve(Request::new(), addr).await?;
         assert_eq!(StatusCode::UNAUTHORIZED, resp.status);
         assert_eq!(
-            INVALID_HEADER_VALUE,
+            INVALID_TOKEN,
             resp.headers[WWW_AUTHENTICATE].to_str()?
         );
 
@@ -144,7 +134,7 @@ mod tests {
         let resp = app.serve(req, addr).await?;
         assert_eq!(StatusCode::UNAUTHORIZED, resp.status);
         assert_eq!(
-            INVALID_HEADER_VALUE,
+            INVALID_TOKEN,
             resp.headers[WWW_AUTHENTICATE].to_str()?
         );
 
@@ -155,7 +145,7 @@ mod tests {
         let resp = app.serve(req, addr).await?;
         assert_eq!(StatusCode::UNAUTHORIZED, resp.status);
         assert_eq!(
-            INVALID_HEADER_VALUE,
+            INVALID_TOKEN,
             resp.headers[WWW_AUTHENTICATE].to_str()?
         );
 
@@ -166,7 +156,7 @@ mod tests {
         let resp = app.serve(req, addr).await?;
         assert_eq!(StatusCode::UNAUTHORIZED, resp.status);
         assert_eq!(
-            INVALID_HEADER_VALUE,
+            INVALID_TOKEN,
             resp.headers[WWW_AUTHENTICATE].to_str()?
         );
 
@@ -183,12 +173,12 @@ mod tests {
         let mut req = Request::new();
         req.headers.insert(
             AUTHORIZATION,
-            format!("Bearer {}", encode(&Header::default(), &user, SECRET)?).parse()?,
+            format!("Bearer {}", encode(&Header::default(), &user, SECRET.as_bytes())?).parse()?,
         );
         let resp = app.serve(req, addr).await?;
         assert_eq!(StatusCode::UNAUTHORIZED, resp.status);
         assert_eq!(
-            INVALID_HEADER_VALUE,
+            INVALID_TOKEN,
             resp.headers[WWW_AUTHENTICATE].to_str()?
         );
 
@@ -198,7 +188,7 @@ mod tests {
             .as_secs(); // one hour later
         req.headers.insert(
             AUTHORIZATION,
-            format!("Bearer {}", encode(&Header::default(), &user, SECRET)?).parse()?,
+            format!("Bearer {}", encode(&Header::default(), &user, SECRET.as_bytes())?).parse()?,
         );
         let resp = app.serve(req, addr).await?;
         assert_eq!(StatusCode::OK, resp.status);
--- src/lib.rs
+++ src/lib.rs
@@ -4,36 +4,19 @@ pub use roa_core::*;
 #[cfg(feature = "query")]
 mod query;
 
+#[cfg(feature = "query")]
+pub use crate::query::{query_parser, Query};
+
 #[cfg(feature = "cookies")]
 mod cookie;
 
-pub mod context {
-    #[cfg(feature = "query")]
-    #[doc(inline)]
-    pub use crate::query::Query;
+#[cfg(feature = "cookies")]
+#[doc(inline)]
+pub use crate::cookie::{cookie_parser, Cookier};
 
-    #[cfg(feature = "cookies")]
-    #[doc(inline)]
-    pub use crate::cookie::Cookier;
+#[cfg(feature = "jwt")]
+mod jwt;
 
-    // #[cfg(feature = "body")]
-    // #[doc(inline)]
-    // pub use roa_body::PowerBody;
-}
-
-pub mod middlewares {
-    #[cfg(feature = "query")]
-    #[doc(inline)]
-    pub use crate::query::query_parser;
-
-    #[cfg(feature = "cookies")]
-    #[doc(inline)]
-    pub use crate::cookie::cookie_parser;
-    // #[cfg(feature = "jwt")]
-    // #[doc(inline)]
-    // pub use roa_jwt::{jwt_verify, JwtVerifier, Validation};
-
-    // #[cfg(feature = "router")]
-    // #[doc(inline)]
-    // pub use roa_router;
-}
+#[cfg(feature = "jwt")]
+#[doc(inline)]
+pub use crate::jwt::{jwt_verify, JwtVerifier};
