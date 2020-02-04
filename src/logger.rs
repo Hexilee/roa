bda991f38152ac84dfebdcad1e7d163e63e36852
Hexilee <hexileee@gmail.com>
2020-02-04 18:16:13 +0800

    pas logger tests

--- src/logger.rs
+++ src/logger.rs
@@ -37,3 +37,78 @@ pub async fn logger<M: Model>(ctx: Context<M>, next: Next) -> Result<(), Status>
     ctx.resp_mut().await.on_finish(callback);
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::logger;
+    use crate::{App, Request};
+    use futures::AsyncReadExt;
+    use http::StatusCode;
+    use lazy_static::lazy_static;
+    use log::{Level, LevelFilter, Metadata, Record, SetLoggerError};
+    use roa_core::throw;
+    use std::sync::RwLock;
+
+    struct TestLogger {
+        records: RwLock<Vec<(String, String)>>,
+    }
+    impl log::Log for TestLogger {
+        fn enabled(&self, metadata: &Metadata) -> bool {
+            metadata.level() <= Level::Info
+        }
+        fn log(&self, record: &Record) {
+            self.records
+                .write()
+                .unwrap()
+                .push((record.level().to_string(), record.args().to_string()))
+        }
+        fn flush(&self) {}
+    }
+
+    lazy_static! {
+        static ref LOGGER: TestLogger = TestLogger {
+            records: RwLock::new(Vec::new()),
+        };
+    }
+
+    fn init() -> Result<(), SetLoggerError> {
+        log::set_logger(&*LOGGER).map(|()| log::set_max_level(LevelFilter::Info))
+    }
+
+    #[tokio::test]
+    async fn log() -> Result<(), Box<dyn std::error::Error>> {
+        init()?;
+
+        // info
+        let mut resp = App::new(())
+            .gate(logger)
+            .gate(move |ctx, _next| async move {
+                ctx.resp_mut().await.write_str("Hello, World.");
+                Ok(())
+            })
+            .serve(Request::new(), "127.0.0.1:8000".parse()?)
+            .await?;
+        let (level, data) = LOGGER.records.write().unwrap().pop().unwrap();
+        assert_eq!("INFO", level);
+        assert_eq!("--> GET /", data);
+        resp.read_to_string(&mut String::new()).await?;
+        let (level, data) = LOGGER.records.write().unwrap().pop().unwrap();
+        assert_eq!("INFO", level);
+        assert_eq!("<-- GET / 0ms 13 B", data);
+
+        // error
+        resp = App::new(())
+            .gate(logger)
+            .gate(move |_ctx, _next| async move { throw(StatusCode::BAD_REQUEST, "Hello, World.") })
+            .serve(Request::new(), "127.0.0.1:8000".parse()?)
+            .await?;
+        let (level, data) = LOGGER.records.write().unwrap().pop().unwrap();
+        assert_eq!("INFO", level);
+        assert_eq!("--> GET /", data);
+        resp.read_to_string(&mut String::new()).await?;
+        let (level, data) = LOGGER.records.write().unwrap().pop().unwrap();
+        assert_eq!("ERROR", level);
+        assert_eq!("<-- GET / 0ms 13 B", data);
+        Ok(())
+    }
+}
