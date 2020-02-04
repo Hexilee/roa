6917962eac8e082b25b4e6a6a765d100af917b7a
Hexilee <hexileee@gmail.com>
2020-02-04 16:59:29 +0800

    fix a typo in serve-file.rs

--- examples/serve-file.rs
+++ examples/serve-file.rs
@@ -14,7 +14,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .gate(compress(Level::Balance))
         .gate(|ctx, _next| async move { ctx.write_file("assets/welcome.html").await })
         .listen("127.0.0.1:8000".parse()?, || {
-            info!("Server is listenning on 127.0.0.1:8000")
+            info!("Server is listening on 127.0.0.1:8000")
         })
         .await
         .map_err(Into::into)
