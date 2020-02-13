14f1babce0e11acc7ef19417be74027feb79262f
Hexilee <hexileee@gmail.com>
2020-02-13 22:25:52 +0800

    complete documenting of module jwt

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,3 +1,69 @@
+//! The jwt module of roa.
+//! This module provides middlewares `guard` and `guard_by`
+//! and a context extension `JwtVerifier`.
+//!
+//! ### Example
+//!
+//! ```rust
+//! use roa::jwt::{guard, JwtVerifier};
+//! use roa::core::{App, StatusCode};
+//! use roa::core::header::AUTHORIZATION;
+//! use async_std::task::spawn;
+//! use jsonwebtoken::{encode, Header};
+//! use serde::{Deserialize, Serialize};
+//! use std::time::{Duration, SystemTime, UNIX_EPOCH};
+//!
+//! #[derive(Debug, Serialize, Deserialize)]
+//! struct User {
+//!     sub: String,
+//!     company: String,
+//!     exp: u64,
+//!     id: u64,
+//!     name: String,
+//! }
+//!
+//! const SECRET: &str = "123456";
+//!
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let mut app = App::new(());
+//!     let (addr, server) = app
+//!         .gate(guard(SECRET))
+//!         .end(move |ctx| async move {
+//!             let user: User = ctx.claims().await?;
+//!             assert_eq!(0, user.id);
+//!             assert_eq!("Hexilee", &user.name);
+//!             Ok(())
+//!         })
+//!         .run_local()?;
+//!     spawn(server);
+//!     let mut user = User {
+//!         sub: "user".to_string(),
+//!         company: "None".to_string(),
+//!         exp: (SystemTime::now() + Duration::from_secs(86400))
+//!             .duration_since(UNIX_EPOCH)?
+//!             .as_secs(), // one second ago
+//!         id: 0,
+//!         name: "Hexilee".to_string(),
+//!     };
+//!
+//!     let client = reqwest::Client::new();
+//!     let resp = client
+//!         .get(&format!("http://{}", addr))
+//!         .header(
+//!             AUTHORIZATION,
+//!             format!(
+//!                 "Bearer {}",
+//!                 encode(&Header::default(), &user, SECRET.as_bytes())?
+//!             ),
+//!         )
+//!         .send()
+//!         .await?;
+//!     assert_eq!(StatusCode::OK, resp.status());
+//!     Ok(())
+//! }
+//! ```
+
 pub use jsonwebtoken::Validation;
 
 use crate::core::header::{HeaderValue, AUTHORIZATION, WWW_AUTHENTICATE};
@@ -11,15 +77,48 @@ const INVALID_TOKEN: &str = r#"Bearer realm="<jwt>", error="invalid_token""#;
 
 struct JwtSymbol;
 
+/// A context extension.
+/// This extension must be used in downstream of middleware `guard` or `guard_by`,
+/// otherwise you cannot get expected claims.
+///
+/// ### Example
+///
+/// ```rust
+/// use roa::core::{Context, Result};
+/// use roa::jwt::JwtVerifier;
+/// use serde_json::Value;
+///
+/// async fn get(ctx: Context<()>) -> Result {
+///     let claims: Value = ctx.claims().await?;
+///     Ok(())
+/// }
+/// ```
 #[async_trait]
 pub trait JwtVerifier<S, C>
 where
     C: 'static + DeserializeOwned,
 {
-    async fn verify(&self, validation: &Validation) -> Result<C>;
+    /// Deserialize claims from token.
     async fn claims(&self) -> Result<C>;
+
+    /// Verify token and deserialize claims with a validation.
+    /// Use this method if this validation is different from that one of guard or guard_by.
+    async fn verify(&self, validation: &Validation) -> Result<C>;
 }
 
+/// Guard by default validation.
+pub fn guard<S: State>(secret: impl ToString) -> impl Middleware<S> {
+    guard_by(secret, Validation::default())
+}
+
+/// Guard downstream.
+///
+/// The json web token should be deliver by request header "authorization",
+/// in this format: `Authorization: Bearer <token>`.
+///
+/// If request is not verified, return 401 UNAUTHORIZED and set response header:
+///
+/// `WWW-Authenticate: Bearer realm="<jwt>", error="invalid_token"`.
 pub fn guard_by<S: State>(secret: impl ToString, validation: Validation) -> impl Middleware<S> {
     join(
         Arc::new(catch_www_authenticate),
@@ -28,10 +127,6 @@ pub fn guard_by<S: State>(secret: impl ToString, validation: Validation) -> impl
             validation,
         },
     )
-}
-
-pub fn guard<S: State>(secret: impl ToString) -> impl Middleware<S> {
-    guard_by(secret, Validation::default())
 }
 
 async fn catch_www_authenticate<S: State>(ctx: Context<S>, next: Next) -> Result {
@@ -80,17 +175,6 @@ where
     S: State,
     C: 'static + DeserializeOwned + Send,
 {
-    async fn verify(&self, validation: &Validation) -> Result<C> {
-        let secret = self.load::<JwtSymbol>("secret").await;
-        let token = self.load::<JwtSymbol>("token").await;
-        match (secret, token) {
-            (Some(secret), Some(token)) => decode(&token, secret.as_bytes(), validation)
-                .map(|data| data.claims)
-                .map_err(unauthorized),
-            _ => Err(guard_not_set()),
-        }
-    }
-
     async fn claims(&self) -> Result<C> {
         let token = self.load::<JwtSymbol>("token").await;
         match token {
@@ -107,6 +191,17 @@ where
                     )
                 }),
             None => Err(guard_not_set()),
+        }
+    }
+
+    async fn verify(&self, validation: &Validation) -> Result<C> {
+        let secret = self.load::<JwtSymbol>("secret").await;
+        let token = self.load::<JwtSymbol>("token").await;
+        match (secret, token) {
+            (Some(secret), Some(token)) => decode(&token, secret.as_bytes(), validation)
+                .map(|data| data.claims)
+                .map_err(unauthorized),
+            _ => Err(guard_not_set()),
         }
     }
 }
