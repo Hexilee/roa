547001d8bc87ec2d0d3e990a49cf13da853f8bf4
Hexilee <hexileee@gmail.com>
2020-02-19 17:27:19 +0800

    add Websocket struct

--- roa-websocket/src/lib.rs
+++ roa-websocket/src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use async_std::net::TcpStream;
+use async_std::sync::Arc;
+pub use async_tungstenite::tungstenite::protocol::WebSocketConfig;
+use async_tungstenite::{accept_async, accept_async_with_config, WebSocketStream};
+use roa_core::{Context, Error, State};
+use std::mem;
+
+pub struct Websocket(WebSocketStream<Arc<TcpStream>>);
+
+impl Websocket {
+    pub fn new<S: State>(ctx: &mut Context<S>) -> Self {
+        let request = mem::take(ctx.req_mut());
+        let stream = ctx.raw_stream();
+        unimplemented!()
     }
 }
