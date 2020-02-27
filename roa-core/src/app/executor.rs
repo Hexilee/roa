53c38ff2b0ac1be5b7de11524f962e8925fbc9bf
Hexilee <hexileee@gmail.com>
2020-02-27 18:44:59 +0800

    add roa_core::app::executor::JoinHandle

--- roa-core/src/app/executor.rs
+++ roa-core/src/app/executor.rs
@@ -1,3 +1,5 @@
+use futures::channel::oneshot::{channel, Receiver};
+use futures::task::{Context, Poll};
 use hyper::rt;
 use std::future::Future;
 use std::pin::Pin;
@@ -22,17 +24,45 @@ pub trait Spawn {
 #[derive(Clone)]
 pub struct Executor(pub Arc<dyn 'static + Send + Sync + Spawn>);
 
+pub struct JoinHandle<T>(Receiver<T>);
+
 impl Executor {
     /// Spawn a future by app runtime
-    pub fn spawn(&self, fut: impl 'static + Send + Future) {
+    pub fn spawn<Fut>(&self, fut: Fut) -> JoinHandle<Fut::Output>
+    where
+        Fut: 'static + Send + Future,
+        Fut::Output: 'static + Send,
+    {
+        let (sender, recv) = channel();
         self.0.spawn(Box::pin(async move {
-            fut.await;
-        }))
+            if let Err(_) = sender.send(fut.await) {
+                // handler is dropped, do nothing.
+            };
+        }));
+        JoinHandle(recv)
     }
 
     /// Spawn a blocking task by app runtime
-    pub fn spawn_blocking(&self, task: impl 'static + Send + FnOnce()) {
-        self.0.spawn_blocking(Box::new(task))
+    pub fn spawn_blocking<T, R>(&self, task: T) -> JoinHandle<R>
+    where
+        T: 'static + Send + FnOnce() -> R,
+        R: 'static + Send,
+    {
+        let (sender, recv) = channel();
+        self.0.spawn_blocking(Box::new(|| {
+            if let Err(_) = sender.send(task()) {
+                // handler is dropped, do nothing.
+            };
+        }));
+        JoinHandle(recv)
+    }
+}
+
+impl<T> Future for JoinHandle<T> {
+    type Output = T;
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let ready = futures::ready!(Pin::new(&mut self.0).poll(cx));
+        Poll::Ready(ready.expect("receiver in JoinHandle shouldn't be canceled"))
     }
 }
 
@@ -42,6 +72,6 @@ where
     F::Output: 'static + Send,
 {
     fn execute(&self, fut: F) {
-        self.spawn(fut)
+        self.spawn(fut);
     }
 }
