d69dc3e97713b1434ebeb4511c75a2c77bc2ed97
Hexilee <hexileee@gmail.com>
2020-02-29 17:01:50 +0800

    pass all tests

--- roa-diesel/src/lib.rs
+++ roa-diesel/src/lib.rs
@@ -1,7 +1,6 @@
 mod async_ext;
 mod err;
 mod pool;
-use err::WrapError;
-
 pub use async_ext::SqlQuery;
+pub use err::WrapError;
 pub use pool::{builder, make_pool, AsyncPool, Pool, WrapConnection};
