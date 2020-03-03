3dafc5eff35d8ccb8dbf77729fd1255cfdac65f8
Hexilee <hexileee@gmail.com>
2020-03-03 14:01:46 +0800

    complete documenting of roa-tcp

--- roa-tcp/src/lib.rs
+++ roa-tcp/src/lib.rs
@@ -1,4 +1,43 @@
+//! This crate provides an incoming implementing `roa_core::Accept` and a app extension.
+//!
+//! ### TcpIncoming
+//!
+//! ```
+//! use roa_core::App;
+//! use roa_tcp::TcpIncoming;
+//! use std::io;
+//!
+//! # fn main() -> io::Result<()> {
+//! let app = App::new(());
+//! let incoming = TcpIncoming::bind("127.0.0.1:0")?;
+//! let server = app.accept(incoming);
+//! // server.await
+//! Ok(())
+//! # }
+//! ```
+//!
+//! ### Listener
+//!
+//! ```
+//! use roa_core::App;
+//! use roa_tcp::Listener;
+//! use std::io;
+//!
+//! # fn main() -> io::Result<()> {
+//! let app = App::new(());
+//! let (addr, server) = app.listen_on("127.0.0.1:0")?;
+//! // server.await
+//! Ok(())
+//! # }
+//! ```
+
+#![warn(missing_docs)]
+
 mod incoming;
 mod listen;
+
+#[doc(inline)]
 pub use incoming::TcpIncoming;
+
+#[doc(inline)]
 pub use listen::Listener;
