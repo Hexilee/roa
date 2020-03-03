612b7a875419e934270bc06bc90ac04400b173b2
Hexilee <hexileee@gmail.com>
2020-03-03 15:06:54 +0800

    complete documenting of roa-multipart

--- roa-multipart/src/lib.rs
+++ roa-multipart/src/lib.rs
@@ -1,3 +1,51 @@
+//! This crate provides a wrapper for `actix_multipart::Multipart`,
+//! which may cause heavy dependencies.
+//!
+//! It won't be used as a module of crate `roa` until implementing a cleaner Multipart.  
+//!
+//! ### Example
+//! ```
+//! use async_std::fs::File;
+//! use async_std::io;
+//! use async_std::path::Path;
+//! use futures::stream::TryStreamExt;
+//! use futures::StreamExt;
+//! use roa::http::StatusCode;
+//! use roa::preload::*;
+//! use roa::router::Router;
+//! use roa::{throw, App};
+//! use roa_multipart::Multipart;
+//! use std::error::Error as StdError;
+//!
+//! # fn main() -> Result<(), Box<dyn StdError>> {
+//! let mut app = App::new(());
+//! let mut router = Router::<()>::new();
+//! router.post("/file", |mut ctx| async move {
+//!     let mut form = Multipart::new(&mut ctx);
+//!     while let Some(item) = form.next().await {
+//!         let field = item?;
+//!         match field.content_disposition() {
+//!             None => throw!(StatusCode::BAD_REQUEST, "content disposition not set"),
+//!             Some(content_disposition) => match content_disposition.get_filename() {
+//!                 None => continue, // ignore non-file field
+//!                 Some(filename) => {
+//!                     let path = Path::new("./upload");
+//!                     let mut file = File::create(path.join(filename)).await?;
+//!                     io::copy(&mut field.into_async_read(), &mut file).await?;
+//!                 }
+//!             },
+//!         }
+//!     }
+//!     Ok(())
+//! });
+//! let (addr, server) = app.run()?;
+//! // server.await
+//! Ok(())
+//! # }
+//! ```
+
+#![warn(missing_docs)]
+
 use actix_http::error::PayloadError;
 use actix_http::http::HeaderMap;
 use actix_multipart::Field as ActixField;
@@ -14,13 +62,21 @@ use std::ops::Deref;
 use std::pin::Pin;
 use std::task::{self, Poll};
 
+/// A wrapper for actix multipart.
 pub struct Multipart(ActixMultipart);
+
+/// A wrapper for actix multipart field.
 pub struct Field(ActixField);
 #[derive(Debug)]
+
+/// A wrapper for actix multipart field.
 pub struct WrapError(MultipartError);
-pub struct WrapStream(Option<Body>);
+
+/// A wrapper for hyper::Body.
+struct WrapStream(Option<Body>);
 
 impl Multipart {
+    /// Construct multipart from Context.
     pub fn new<S: State>(ctx: &mut Context<S>) -> Self {
         let mut map = HeaderMap::new();
         if let Some(value) = ctx.req().headers.get(CONTENT_TYPE) {
@@ -35,6 +91,7 @@ impl Multipart {
 
 impl Stream for WrapStream {
     type Item = Result<Bytes, PayloadError>;
+
     fn poll_next(
         mut self: Pin<&mut Self>,
         cx: &mut task::Context<'_>,
@@ -127,3 +184,80 @@ impl Display for WrapError {
 }
 
 impl std::error::Error for WrapError {}
+
+#[cfg(test)]
+mod tests {
+    use super::Multipart;
+    use async_std::fs::{read, read_to_string};
+    use futures::stream::TryStreamExt;
+    use futures::{AsyncReadExt, StreamExt};
+    use reqwest::{
+        multipart::{Form, Part},
+        Client,
+    };
+    use roa::http::{header::CONTENT_TYPE, StatusCode};
+    use roa::preload::*;
+    use roa::router::Router;
+    use roa::{throw, App};
+    use std::error::Error as StdError;
+
+    const FILE_PATH: &str = "../assets/author.txt";
+    const FILE_NAME: &str = "author.txt";
+    const FIELD_NAME: &str = "file";
+
+    #[tokio::test]
+    async fn upload() -> Result<(), Box<dyn StdError>> {
+        let mut app = App::new(());
+        let mut router = Router::<()>::new();
+        router.post("/file", |mut ctx| async move {
+            let mut form = Multipart::new(&mut ctx);
+            while let Some(item) = form.next().await {
+                let field = item?;
+                match field.content_disposition() {
+                    None => {
+                        throw!(StatusCode::BAD_REQUEST, "content disposition not set")
+                    }
+                    Some(disposition) => {
+                        match (disposition.get_filename(), disposition.get_name()) {
+                            (Some(filename), Some(name)) => {
+                                assert_eq!(FIELD_NAME, name);
+                                assert_eq!(FILE_NAME, filename);
+                                let mut content = String::new();
+                                field
+                                    .into_async_read()
+                                    .read_to_string(&mut content)
+                                    .await?;
+                                let expected_content = read_to_string(FILE_PATH).await?;
+                                assert_eq!(&expected_content, &content);
+                            }
+                            _ => throw!(StatusCode::BAD_REQUEST, "invalid field"),
+                        }
+                    }
+                }
+            }
+            Ok(())
+        });
+        let (addr, server) = app.gate(router.routes("/")?).run()?;
+        async_std::task::spawn(server);
+
+        // client
+        let url = format!("http://{}/file", addr);
+        let client = Client::new();
+        let form = Form::new().part(
+            FIELD_NAME,
+            Part::bytes(read(FILE_PATH).await?).file_name(FILE_NAME),
+        );
+        let boundary = form.boundary().to_string();
+        let resp = client
+            .post(&url)
+            .body(form.stream())
+            .header(
+                CONTENT_TYPE,
+                format!(r#"multipart/form-data; boundary="{}""#, boundary),
+            )
+            .send()
+            .await?;
+        assert_eq!(StatusCode::OK, resp.status());
+        Ok(())
+    }
+}
