b994521a12ee1acf4bc9e1fbae2c3fda13b1ae78
Hexilee <hexileee@gmail.com>
2020-03-04 11:50:45 +0800

    add echo example

---
+++ examples/echo.rs
@@ -0,0 +1,20 @@
+use log::info;
+use roa::preload::*;
+use roa::App;
+use std::error::Error as StdError;
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn StdError>> {
+    pretty_env_logger::init();
+    let mut app = App::new(());
+    app.end(|mut ctx| async move {
+        let stream = ctx.req_mut().stream();
+        ctx.resp_mut().write_stream(stream);
+        Ok(())
+    })
+    .listen("127.0.0.1:8000", |addr| {
+        info!("Server is listening on {}", addr)
+    })?
+    .await?;
+    Ok(())
+}
