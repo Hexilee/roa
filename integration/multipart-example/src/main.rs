b84d186794deed58c2147eecd42cafe5c07131c6
Hexilee <hexileee@gmail.com>
2020-03-14 21:23:21 +0800

    should fix multipart-example

--- integration/multipart-example/src/main.rs
+++ integration/multipart-example/src/main.rs
@@ -8,40 +8,43 @@ use roa::body::{DispositionType, PowerBody};
 use roa::http::StatusCode;
 use roa::logger::logger;
 use roa::preload::*;
-use roa::router::Router;
-use roa::{throw, App};
+use roa::router::{get, post, Router};
+use roa::{throw, App, Context};
 use roa_multipart::Multipart;
 use std::error::Error as StdError;
+
+async fn get_form(ctx: &mut Context<()>) -> roa::Result {
+    ctx.write_file("./assets/index.html", DispositionType::Inline)
+        .await
+}
+
+async fn post_file(ctx: &mut Context<()>) -> roa::Result {
+    let mut form = Multipart::new(ctx);
+    while let Some(item) = form.next().await {
+        let field = item?;
+        info!("{}", field.content_type());
+        match field.content_disposition() {
+            None => throw!(StatusCode::BAD_REQUEST, "content disposition not set"),
+            Some(content_disposition) => match content_disposition.get_filename() {
+                None => continue, // ignore non-file field
+                Some(filename) => {
+                    let path = Path::new("./upload");
+                    let mut file = File::create(path.join(filename)).await?;
+                    io::copy(&mut field.into_async_read(), &mut file).await?;
+                }
+            },
+        }
+    }
+    Ok(())
+}
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn StdError>> {
     pretty_env_logger::init();
-    let mut app = App::new(());
-    let mut router = Router::<()>::new();
-    router.get("/", |mut ctx| async move {
-        ctx.write_file("./assets/index.html", DispositionType::Inline)
-            .await
-    });
-    router.post("/file", |mut ctx| async move {
-        let mut form = Multipart::new(&mut ctx);
-        while let Some(item) = form.next().await {
-            let field = item?;
-            info!("{}", field.content_type());
-            match field.content_disposition() {
-                None => throw!(StatusCode::BAD_REQUEST, "content disposition not set"),
-                Some(content_disposition) => match content_disposition.get_filename() {
-                    None => continue, // ignore non-file field
-                    Some(filename) => {
-                        let path = Path::new("./upload");
-                        let mut file = File::create(path.join(filename)).await?;
-                        io::copy(&mut field.into_async_read(), &mut file).await?;
-                    }
-                },
-            }
-        }
-        Ok(())
-    });
-    app.gate(logger).gate(router.routes("/")?);
+    let router = Router::new()
+        .on("/", get(get_form))
+        .on("/file", post(post_file));
+    let app = App::new(()).gate(logger).end(router.routes("/")?);
     app.listen("127.0.0.1:8000", |addr| {
         info!("Server is listening on {}", addr)
     })?
