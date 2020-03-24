c9d489fd3f1e5eb699a94bf7495fa8731020e560
Hexilee <hexileee@gmail.com>
2020-03-25 01:08:38 +0800

    pg example pass compiling

--- roa/src/pg.rs
+++ roa/src/pg.rs
@@ -3,17 +3,50 @@
 //! ### Example
 //!
 //! ```rust,no_run
-//! use roa::{App, Context, Result};
+//! use roa::{App, Context, throw};
+//! use roa::http::StatusCode;
 //! use roa::pg::{connect, Client};
 //! use std::sync::Arc;
+//! use std::error::Error;
+//! use roa::query::query_parser;
+//! use roa::preload::*;
+//! use async_std::task::spawn;
 //!
 //! #[derive(Clone)]
 //! struct State {
 //!     pg: Arc<Client>
 //! }
 //!
-//! async fn query(ctx: &mut Context<State>) -> Result {
-//!     ctx.pg.query_one("SELECT * FROM user WHERE id=$1", &[&1]).await?;
+//! impl State {
+//!     pub async fn new(pg_url: &str) -> Result<Self, Box<dyn Error>> {
+//!         let (client, conn) = connect(&pg_url.parse()?).await?;
+//!         spawn(conn);
+//!         Ok(Self {pg: Arc::new(client)})
+//!     }
+//! }
+//!
+//! async fn query(ctx: &mut Context<State>) -> roa::Result {
+//!     let id: u32 = ctx.must_query("id")?.parse()?;
+//!     match ctx.pg.query_opt("SELECT * FROM user WHERE id=$1", &[&id]).await? {
+//!         Some(row) => {
+//!             let value: String = row.get(0);
+//!             ctx.write_text(value);
+//!             Ok(())
+//!         }         
+//!         None => throw!(StatusCode::NOT_FOUND),
+//!     }
+//! }
+//!
+//! #[async_std::main]
+//! async fn main() -> Result<(), Box<dyn Error>> {
+//!     let url = "host=localhost user=postgres";
+//!     let state = State::new(url).await?;
+//!     App::new(state)
+//!         .gate(query_parser)
+//!         .end(query)
+//!         .listen("127.0.0.1:0", |addr| {
+//!             println!("Server is listening on {}", addr)
+//!         })?.await?;
 //!     Ok(())
 //! }
 //! ```
