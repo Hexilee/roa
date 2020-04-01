8ab5bfe19e94c961c9fd2b89f4c360104c8832ff
Hexilee <hexileee@gmail.com>
2020-04-01 12:00:20 +0800

    release roa-core v0.5.0-rc.4

--- roa-core/src/body.rs
+++ roa-core/src/body.rs
@@ -1,7 +1,7 @@
 use bytes::{Buf, Bytes, BytesMut};
 use futures::future::ok;
 use futures::io::{self, AsyncRead};
-use futures::stream::{once, Stream};
+use futures::stream::{once, Stream, StreamExt};
 use std::mem;
 use std::pin::Pin;
 use std::task::{Context, Poll};
@@ -40,17 +40,14 @@ pub enum Body {
     Once(Bytes),
 
     /// Stream kind.
-    Stream(BodyStream),
+    Stream(Segment),
 }
 
-/// Stream based body.
+/// A boxed stream.
 #[derive(Default)]
-pub struct BodyStream {
-    counter: usize,
-    segments: Vec<Segment>,
-}
-
-type Segment = Box<dyn Stream<Item = io::Result<Bytes>> + Sync + Send + Unpin + 'static>;
+pub struct Segment(
+    Option<Pin<Box<dyn Stream<Item = io::Result<Bytes>> + Sync + Send + 'static>>>,
+);
 
 impl Body {
     /// Construct an empty body.
@@ -67,34 +64,32 @@ impl Body {
 
     /// Construct an empty body of stream kind.
     #[inline]
-    pub fn stream() -> Self {
-        Body::Stream(BodyStream {
-            counter: 0,
-            segments: Vec::new(),
-        })
+    pub fn stream<S>(stream: S) -> Self
+    where
+        S: Stream<Item = io::Result<Bytes>> + Sync + Send + 'static,
+    {
+        Body::Stream(Segment::new(stream))
     }
 
     /// Write stream.
+    #[inline]
     pub fn write_stream(
         &mut self,
-        stream: impl Stream<Item = io::Result<Bytes>> + Sync + Send + Unpin + 'static,
+        stream: impl Stream<Item = io::Result<Bytes>> + Sync + Send + 'static,
     ) -> &mut Self {
         match self {
             Body::Empty => {
-                *self = Self::stream();
-                self.write_stream(stream)
+                *self = Self::stream(stream);
             }
             Body::Once(bytes) => {
-                let data = mem::take(bytes);
-                *self = Self::stream();
-                self.write(data);
-                self.write_stream(stream)
+                let stream = once(ok(mem::take(bytes))).chain(stream);
+                *self = Self::stream(stream);
             }
-            Body::Stream(body_stream) => {
-                body_stream.write_stream(stream);
-                self
+            Body::Stream(segment) => {
+                *self = Self::stream(mem::take(segment).chain(stream));
             }
         }
+        self
     }
 
     /// Write reader with default chunk size.
@@ -117,36 +112,24 @@ impl Body {
     }
 
     /// Write `Bytes`.
+    #[inline]
     pub fn write(&mut self, data: impl Into<Bytes>) -> &mut Self {
         match self {
             Body::Empty => {
                 *self = Self::once(data.into());
+                self
             }
-            Body::Once(bytes) => {
-                let mut stream = Self::stream();
-                stream.write(mem::take(bytes));
-                stream.write(data.into());
-                *self = stream;
-            }
-            Body::Stream(stream) => stream.write(data),
+            body => body.write_stream(once(ok(data.into()))),
         }
-        self
     }
 }
 
-impl BodyStream {
-    /// Write stream.
+impl Segment {
     #[inline]
-    fn write_stream(
-        &mut self,
-        stream: impl Stream<Item = io::Result<Bytes>> + Sync + Send + Unpin + 'static,
-    ) {
-        self.segments.push(Box::new(stream))
-    }
-
-    #[inline]
-    fn write(&mut self, bytes: impl Into<Bytes>) {
-        self.write_stream(once(ok(bytes.into())))
+    fn new(
+        stream: impl Stream<Item = io::Result<Bytes>> + Sync + Send + 'static,
+    ) -> Self {
+        Self(Some(Box::pin(stream)))
     }
 }
 
@@ -203,26 +186,6 @@ where
     }
 }
 
-impl Stream for BodyStream {
-    type Item = io::Result<Bytes>;
-    fn poll_next(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-    ) -> Poll<Option<Self::Item>> {
-        let counter = self.counter;
-        if counter >= self.segments.len() {
-            return Poll::Ready(None);
-        }
-        match futures::ready!(Pin::new(&mut self.segments[counter]).poll_next(cx)) {
-            None => {
-                self.counter += 1;
-                self.poll_next(cx)
-            }
-            some => Poll::Ready(some),
-        }
-    }
-}
-
 impl Stream for Body {
     type Item = io::Result<Bytes>;
     #[inline]
@@ -238,6 +201,20 @@ impl Stream for Body {
                 Poll::Ready(Some(Ok(data)))
             }
             Body::Stream(stream) => Pin::new(stream).poll_next(cx),
+        }
+    }
+}
+
+impl Stream for Segment {
+    type Item = io::Result<Bytes>;
+    #[inline]
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        match self.0 {
+            None => Poll::Ready(None),
+            Some(ref mut stream) => stream.as_mut().poll_next(cx),
         }
     }
 }
@@ -280,7 +257,7 @@ mod tests {
 
     #[async_std::test]
     async fn body_composed() -> std::io::Result<()> {
-        let mut body = Body::stream();
+        let mut body = Body::empty();
         body.write("He")
             .write("llo, ")
             .write_reader(File::open("../assets/author.txt").await?)
